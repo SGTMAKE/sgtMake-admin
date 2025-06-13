fb210d8566dec2cf6b49d8286d3d911629234cb0
Rishabh <rishabh@arevei.com>
2025-06-13 19:56:18 +0530

    statics

--- src/app/dashboard/services/page.tsx
+++ src/app/dashboard/services/page.tsx
@@ -1,10 +1,8 @@
-import { getServicesServer } from "@/lib/services/get-services"
 import Tabs from "@/components/dashboard/services/tabs"
 import ServicesPage from "@/components/dashboard/services/services-page"
 import Nav from "@/components/nav/nav"
 
 export default async function ServicesPageContainer() {
-  await getServicesServer() // Prefetch data for hydration
 
   return (
       <Nav>
