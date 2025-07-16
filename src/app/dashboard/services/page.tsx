7421d0da87583269bbd6754311de824b87734715
Rishabh <rishabh@arevei.com>
2025-07-16 21:19:21 +0530

    caching1

--- src/app/dashboard/services/page.tsx
+++ src/app/dashboard/services/page.tsx
@@ -3,13 +3,10 @@ import ServicesPage from "@/components/dashboard/services/services-page"
 import Nav from "@/components/nav/nav"
 
 export default async function ServicesPageContainer() {
-
   return (
       <Nav>
     <div className="container mx-auto px-4 py-8">
-      <Tabs>
         <ServicesPage />
-      </Tabs>
     </div>
       </Nav>
   )
